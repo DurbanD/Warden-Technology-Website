ab9e6123c51af14eff9530d22454cad28fc2f25f
Durban <durban.doyle@gmail.com>
2026-03-07 19:21:53 -0500

    Coming Soon Website First Commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import ComingSoon from './components/ComingSoon.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/WT Logo White.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <ComingSoon msg="Coming Soon..." />
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
---
+++ src/components/__tests__/ComingSoon.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import ComingSoon from '../ComingSoon.vue'
+
+describe('ComingSoon', () => {
+  it('renders properly', () => {
+    const wrapper = mount(ComingSoon, { props: { msg: 'Hello Vitest' } })
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
+})
